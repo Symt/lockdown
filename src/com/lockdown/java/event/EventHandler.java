23d770af4735481f8fbd9fb6a883be0bee1e2de8
symt <xxrequired@gmail.com>
2019-01-11 18:49:02 -0600

    File Downloader

--- src/com/lockdown/java/event/EventHandler.java
+++ src/com/lockdown/java/event/EventHandler.java
@@ -1,18 +1,52 @@
 package com.lockdown.java.event;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+
 public final class EventHandler {
-	
+
 	public static void passEvent(String event, boolean repeats, long repeatDelay, int iterations) {
-		Trigger t = () -> {
-			
-		};
-		switch(event) {
-		/*
-		 * Event handling will happen here
-		 */
+		Trigger t = null;
+		switch (event) {
+		case "{download_file}":
+			t = () -> {
+				//TODO: Allow for parameter input for file url and file name
+				FileDownloader fd = new FileDownloader("", "");
+				fd.downloadFile();
+			};
+			break;
+			default:
+				t = () -> {
+				};
 		}
 		Event e = new Event(event, repeats, repeatDelay, iterations, t);
 		e.execute();
 	}
 
+}
+
+class FileDownloader {
+	public static boolean isDownloading;
+	public final String fileLink;
+	public final String fileName;
+	public final File file;
+
+	public FileDownloader(String link, String fileName) {
+		fileLink = link;
+		this.fileName = fileName;
+		file = new File(System.getProperty("user.home") + "/Downloads/" + fileName);
+	}
+
+	public void downloadFile() {
+		try {
+			URL downloadUrl = new URL(fileLink);
+			file.createNewFile();
+			FileUtils.copyURLToFile(downloadUrl, file, 0, 0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
