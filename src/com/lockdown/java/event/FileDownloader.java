e56afa6fb026bd28876979a79ca61bcdb18da1bc
bdogshadybanks <bdogshadybanks@gmail.com>
2019-01-12 10:41:50 -0600

    Update FileDownloader.java

--- src/com/lockdown/java/event/FileDownloader.java
+++ src/com/lockdown/java/event/FileDownloader.java
@@ -10,11 +10,14 @@ public class FileDownloader {
 	public static boolean isDownloading;
 	public final String fileLink;
 	public final String fileName;
+	public final String downloadDirectory; 
 	public final File file;
+	
 
 	public FileDownloader(String link, String fileName, String downloadDirectory) {
 		fileLink = link;
 		this.fileName = fileName;
+		this.downloadDirectory =  downloadDirectory;
 		file = new File(downloadDirectory + fileName);
 	}
 
@@ -34,5 +37,37 @@ public class FileDownloader {
 		/*
 		 * If the directory is /tmp/ and the file is either a .app or a .pkg, allow the file to be ran and installed to /Applications (or ~/Applications for now).
 		 */
+		if(downloadDirectory.substring(downloadDirectory.length()-5).equals("/tmp/"))
+		{
+			if(fileName.substring(fileName.length()-4).equals(".app")||fileName.substring(fileName.length()-4).equals(".pkg"))
+			{
+				if(fileName.substring(fileName.length()-4).equals(".app"))
+				{
+					//TODO: Implementation
+					
+					/*
+					 *  Code to install app
+					 */
+				}
+				else
+				{
+					//TODO: Implementation
+					
+					/*
+					 * Code to install pkg
+					 */
+				}
+			}
+		}
 	}
-}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
