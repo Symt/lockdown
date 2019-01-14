79865e99c87dc3a8b8cd3d365721b71d94dc8724
symt <xxrequired@gmail.com>
2019-01-14 08:55:16 -0600

    Moved CSS and JS to separate files

---
+++ src/com/lockdown/html/script.js
@@ -0,0 +1,66 @@
+function start() {
+    homestart().then(function () {
+        percent = 0;
+        document.getElementById("hr").style.display = "block";
+        hrstart().then(function cb() {
+            setTimeout(function () {
+                if (percent < 101) {
+                    cb();
+                } else {
+                    document.getElementById("title").style.display = "block";
+                }
+            }, 10);
+        });
+    });
+}
+
+function update(location, self) {
+    var items = document.getElementsByClassName("navbutton");
+    for (var i = 0; i < items.length; i++) {
+        items[i].classList.remove("active");
+    }
+    self.classList.add("active");
+    if (location == "Home") {
+        document.getElementById("home").style.display = "block";
+        document.getElementById("applications").style.display = "none";
+        document.getElementById("commands").style.display = "none";
+        document.getElementById("settings").style.display = "none";
+    } else if (location == "Applications") {
+        document.getElementById("home").style.display = "none";
+        document.getElementById("applications").style.display = "block";
+        document.getElementById("commands").style.display = "none";
+        document.getElementById("settings").style.display = "none";
+    } else if (location == "Settings") {
+        document.getElementById("home").style.display = "none";
+        document.getElementById("applications").style.display = "none";
+        document.getElementById("commands").style.display = "none";
+        document.getElementById("settings").style.display = "block";
+    } else if (location == "Commands") {
+        document.getElementById("home").style.display = "none";
+        document.getElementById("applications").style.display = "none";
+        document.getElementById("commands").style.display = "block";
+        document.getElementById("settings").style.display = "none";
+    }
+}
+
+async function homestart() {
+    setTimeout(function () {
+        if (percent == 26) {
+            return;
+        }
+        document.getElementById("lockdown-center").style.height = percent + "vw";
+        percent++;
+        homestart();
+    }, 10);
+}
+
+async function hrstart() {
+    setTimeout(function () {
+        if (percent == 101) {
+            return;
+        }
+        document.getElementById("hr").style.width = percent + "%";
+        percent++;
+        hrstart();
+    }, 10);
+}
