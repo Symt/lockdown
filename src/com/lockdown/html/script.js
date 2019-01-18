b47b4ae22bc394e5ba9de7d09bfc2e988dbe0460
symt <xxrequired@gmail.com>
2019-01-17 19:42:14 -0600

    "JQuerified" script.js

--- src/com/lockdown/html/script.js
+++ src/com/lockdown/html/script.js
@@ -1,5 +1,5 @@
-function start(num,paths,names) {
-    add(num,paths,names);
+function start(num, paths, names) {
+    add(num, paths, names);
     homestart().then(function () {
         percent = 0;
         document.getElementById("hr").style.display = "block";
@@ -8,7 +8,7 @@ function start(num,paths,names) {
                 if (percent < 101) {
                     cb();
                 } else {
-                    document.getElementById("title").style.display = "block";
+                    $("#title").css("display", "block");
                 }
             }, 10);
         });
@@ -22,36 +22,29 @@ function update(location, self) {
     }
     self.classList.add("active");
     if (location == "Home") {
-        document.getElementById("home").style.display = "block";
-        document.getElementById("applications").style.display = "none";
-        document.getElementById("commands").style.display = "none";
-        document.getElementById("settings").style.display = "none";
-        document.getElementById("downloads").style.display = "none";
+        hideAll();
+        $("#home").css("display", "block");
     } else if (location == "Applications") {
-        document.getElementById("home").style.display = "none";
-        document.getElementById("applications").style.display = "block";
-        document.getElementById("commands").style.display = "none";
-        document.getElementById("settings").style.display = "none";
-        document.getElementById("downloads").style.display = "none";
+        hideAll();
+        $("#applications").css("display", "block");
     } else if (location == "Settings") {
-        document.getElementById("home").style.display = "none";
-        document.getElementById("applications").style.display = "none";
-        document.getElementById("commands").style.display = "none";
-        document.getElementById("settings").style.display = "block";
-        document.getElementById("downloads").style.display = "none";
+        hideAll();
+        $("#settings").css("display", "block");
     } else if (location == "Commands") {
-        document.getElementById("home").style.display = "none";
-        document.getElementById("applications").style.display = "none";
-        document.getElementById("commands").style.display = "block";
-        document.getElementById("settings").style.display = "none";
-        document.getElementById("downloads").style.display = "none";
+        hideAll();
+        $("#commands").css("display", "block");
     } else if (location == "Downloads") {
-        document.getElementById("home").style.display = "none";
-        document.getElementById("applications").style.display = "none";
-        document.getElementById("commands").style.display = "none";
-        document.getElementById("settings").style.display = "none";
-        document.getElementById("downloads").style.display = "block";
+        hideAll();
+        $("#downloads").css("display", "block");
     }
+}
+
+function hideAll() {
+    $("#home").css("display", "none");
+    $("#applications").css("display", "none");
+    $("#commands").css("display", "none");
+    $("#settings").css("display", "none");
+    $("#downloads").css("display", "none");
 }
 
 async function homestart() {
@@ -59,7 +52,7 @@ async function homestart() {
         if (percent == 26) {
             return;
         }
-        document.getElementById("lockdown-center").style.height = percent + "vw";
+        $("#lockdown-center").css("height", percent + "vw");
         percent++;
         homestart();
     }, 10);
@@ -70,15 +63,14 @@ async function hrstart() {
         if (percent == 101) {
             return;
         }
-        document.getElementById("hr").style.width = percent + "%";
+        $("#hr").css("width", percent + "%");
         percent++;
         hrstart();
     }, 10);
 }
 
-function add(num,paths,names) {
-    var buttonDiv = document.getElementById("downloads");
+function add(num, paths, names) {
     for (var i = 1; i <= num; i++) {
-        buttonDiv.innerHTML += '<button type="button" class="big-btn" href="{download_file|/Desktop/|'+paths[i-1]+'|'+names[i-1]+'}">' + names[i-1] + '</button>';
+        $("#downloads").html('<button type="button" class="big-btn" href="{download_file|/Desktop/|' + paths[i - 1] + '|' + names[i - 1] + '}">' + names[i - 1] + '</button>');
     }
 }
