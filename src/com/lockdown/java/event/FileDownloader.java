403e2d72803c5e12964663375781ab8afa5e6e04
bdogshadybanks <bdogshadybanks@gmail.com>
2019-02-14 12:26:50 -0600

    Delete method added to file downloader

--- src/com/lockdown/java/event/FileDownloader.java
+++ src/com/lockdown/java/event/FileDownloader.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import java.net.URL;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
 
 public class FileDownloader {
 	public static boolean isDownloading;
@@ -28,5 +32,30 @@ public class FileDownloader {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		String source = downloadDirectory + fileName;
+		if (fileName.substring(fileName.length() - 4).equals(".zip")) {
+
+			String destination = System.getProperty("user.home") + "/Applications/";
+			try {
+				ZipFile zipFile = new ZipFile(source);
+				java.util.zip.ZipFile entries = new java.util.zip.ZipFile(source);
+				String zipName = StringUtils.split(entries.entries().nextElement().getName(), "/")[0];
+				entries.close();
+				zipFile.extractAll(destination);
+				String path = destination + zipName + "/Contents/MacOS/";
+				path += new File(path).list()[0];
+				EventHandler.passEvent("execute_command|chmod +x " + path, false, 1000, -1);
+
+			} catch (ZipException | IOException e) {
+				e.printStackTrace();
+			}
+		} else if (fileName.substring(fileName.length() - 4).equals(".pkg")) {
+			EventHandler.passEvent("execute_command|installer -pkg " + source + " -target ~", false, 1000,
+					-1);
+		}
+		
+		file.delete();
 	}
+	
 }
